286b2bcb1356721ba10a3551f4bd392f0befdf37
Benjamin Jones <bljones36@gmail.com>
2018-11-13 02:28:59 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -2,7 +2,7 @@ class CreateActors < ActiveRecord::Migration
   def change
     create_table :actors do |t| #we get a block variable here for the table
       t.string :first_name
-      t.integer :last_name
+      t.string :last_name
     end
   end
 end
